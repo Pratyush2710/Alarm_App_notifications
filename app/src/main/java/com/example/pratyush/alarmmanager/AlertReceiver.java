f326cc4ebfbef508ace50693cd9c102df0dea908
Pratyush Garg <pratyushhg@gmail.com>
2018-11-06 12:40:42 +0530

    Alarm tone set, now rings the DEFAULT RINGTONE OF PHONE on Alarm trigger

--- app/src/main/java/com/example/pratyush/alarmmanager/AlertReceiver.java
+++ app/src/main/java/com/example/pratyush/alarmmanager/AlertReceiver.java
@@ -3,6 +3,8 @@ package com.example.pratyush.alarmmanager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
 
 public class  AlertReceiver extends BroadcastReceiver {
@@ -13,5 +15,8 @@ public class  AlertReceiver extends BroadcastReceiver {
         NotificationCompat.Builder nb=notificationhelper.getchannelNotification("Alarm",
                 "Time to close your Module:- MODULE CLOSE :-)");
         notificationhelper.getManager().notify(1,nb.build());
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(context, Settings.System.DEFAULT_RINGTONE_URI);
+        mediaPlayer.start();
     }
 }
