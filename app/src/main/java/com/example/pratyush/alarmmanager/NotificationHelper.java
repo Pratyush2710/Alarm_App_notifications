05f7acddbcba229bbf599e033e7c4eea11724655
Pratyush Garg <pratyushhg@gmail.com>
2018-11-06 14:10:14 +0530

    AutoCancel of notification when cancelled

--- app/src/main/java/com/example/pratyush/alarmmanager/NotificationHelper.java
+++ app/src/main/java/com/example/pratyush/alarmmanager/NotificationHelper.java
@@ -5,8 +5,10 @@ import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 
@@ -38,10 +40,16 @@ public class NotificationHelper extends ContextWrapper {
             return mmanager;
         }
         public android.support.v4.app.NotificationCompat.Builder getchannelNotification(String title,String message){
+            Intent resultIntent = new Intent(this, MainActivity.class);
+            PendingIntent resultpendingIntent = PendingIntent.getActivity(this, 1, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
             return new android.support.v4.app.NotificationCompat.Builder(getApplicationContext(),channelid)
                     .setContentTitle(title)
                     .setContentText(message)
-                    .setSmallIcon(R.drawable.ic_one);
+                    .setSmallIcon(R.drawable.ic_one)
+                    .setAutoCancel(true)
+                    .setContentIntent(resultpendingIntent);
         }
     }
 
